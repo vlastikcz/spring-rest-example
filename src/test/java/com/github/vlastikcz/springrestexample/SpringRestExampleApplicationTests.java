996e8b6318a023a89552a189635c62c7493b8f78
Kotas Vlastimil <Vlastimil.Kotas@ysoft.com>
2017-06-28 18:13:29 +0200

    Scaffold new Spring HATEOAS project

---
+++ src/test/java/com/github/vlastikcz/springrestexample/SpringRestExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.vlastikcz.springrestexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
